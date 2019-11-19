fb2e5711606aa0cd7c76a8db6d876da9dccbb3b9
boscohogans <davidstephenkennedy@gmail.com>
2019-11-19 19:14:55 +0000

    Fixed data frame issue

--- propensity_score_match.R
+++ propensity_score_match.R
@@ -1,9 +1,9 @@
 
-setwd("C:/Users/idcvdken/Dropbox (LSoHaTM)/DK/Fisherpeople/Data/DHS/FileOut")
+#setwd("C:/Users/idcvdken/Dropbox (LSoHaTM)/DK/Fisherpeople/Data/DHS/FileOut")
 
 #Read in dataset
 library(readstata13)
-hr <- read.dta13("gps_hr_20191119_dk.dta")
+hr <- read.dta13("data//gps_hr_20191119_dk.dta")
 
 library(tidyverse)
 #Make new cluster variable
@@ -12,20 +12,19 @@ hr$clusters <- paste(hr$hv000, hr$hv021, sep="_")
 #Change fishing indicator to factor 
 hr$fish <- as.factor(hr$fishing_community)
 
-
 hr1_sum <- 
   hr %>%
   filter(hv000 %in% c("KE6", "MW7", "TZ7", "UG7", "ZM6")) %>%
-  select(hv000, hhid, clusters, fishing_community,
+  select(hv000, hhid, fish, clusters, fishing_community,
          num_hh_members,quintile_nowashnomat,country_) %>%
-    group_by(clusters) %>%
+    group_by(clusters,fish) %>%
     dplyr::summarise(median_hhmembers=median(num_hh_members),
                    median_quintile=median(quintile_nowashnomat),
-                   fishing_community=max(fishing_community),
                    country_=first(country_),na.rm=TRUE)
 
 library(MatchIt)
 
+hr1_sum <- as.data.frame(hr1_sum)
 #Match bases on hhmembers,quintile,country
 hr1_match <- matchit(fish ~ median_hhmembers + median_quintile + country_,
                 data = hr1_sum, method = "nearest",
@@ -40,15 +39,13 @@ hr2_sum <-
   hr %>%
   filter(hv000 %in% c("KE5", "MW5", "TZ5", "UG6", "ZM5")) %>%
   select(hv000, hhid, clusters, fishing_community,
-         num_hh_members,quintile_nowashnomat,country_) %>%
-  group_by(clusters) %>%
+         num_hh_members,fish,quintile_nowashnomat,country_) %>%
+  group_by(clusters, fish) %>%
   dplyr::summarise(median_hhmembers=median(num_hh_members),
                    median_quintile=median(quintile_nowashnomat),
-                   fishing_community=max(fishing_community),
                    country_=first(country_),na.rm=TRUE)
 
-
-
+hr2_sum <- as.data.frame(hr2_sum)
 #Match bases on hhmembers,quintile,country
 hr2_match <- matchit(fish ~ median_hhmembers + median_quintile + country_,
                      data = hr2_sum, method = "nearest",
@@ -60,4 +57,4 @@ hr2_cc <- match.data(hr2_match)
 
 all_data <- rbind(hr1_cc, hr2_cc)
 
-write.csv(all_data,"propensity_score_matching_20191119.csv")
+write.csv(all_data,"data//propensity_score_matching_20191119.csv")
